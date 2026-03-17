359160cc538da0c007248e717b89e7a7c31d6d22
Alex.Zhang <alexchenxizhang@163.com>
2026-03-17 19:39:11 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   // 核心配置：开启静态导出
   basePath: '/github-workflows-demo',
-  assetPrefix: '/github-workflows-demo',
+  assetPrefix: '/github-workflows-demo/',
   output: "export",
 
   // 可选配置：如果你不需要图片优化（静态站必须关闭）
@@ -12,6 +12,7 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  staticPageGenerationTimeout: 1000,
 };
 
 export default nextConfig;
