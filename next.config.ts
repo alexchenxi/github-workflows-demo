0497e50cc9c1d9f7b1699e964157220213890dd3
Alex.Zhang <alexchenxizhang@163.com>
2026-03-17 19:35:00 +0800

    feat(next): 添加 basePath 和 assetPrefix 配置

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // 核心配置：开启静态导出
+  basePath: '/github-workflows-demo',
+  assetPrefix: '/github-workflows-demo',
   output: "export",
 
   // 可选配置：如果你不需要图片优化（静态站必须关闭）
   images: {
     unoptimized: true,
   },
-  trailingSlash: true, 
+  trailingSlash: true,
 };
 
 export default nextConfig;
