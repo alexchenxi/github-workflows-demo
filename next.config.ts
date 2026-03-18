87e7f052d60d78ccb98b76e792871494d4e970b4
alex-zhang_1701 <alex.zhang@take2games.com>
2026-03-18 09:50:35 +0800

    fa

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // 固定 GitHub Pages 子路径（和仓库名一致）
-  basePath: "/github-workflows-demo",
+  // 固定子路径（和仓库名完全一致）
+  basePath: '/github-workflows-demo',
+  // 静态资源前缀（末尾必须加 /，适配子路径）
+  assetPrefix: '/github-workflows-demo/',
 
-  // 静态资源前缀必须与 basePath 一致
-  assetPrefix: "/github-workflows-demo",
-
-  // 开启静态导出
-  output: "export",
-  trailingSlash: true, // 添加此行
-
-  // 关闭图片优化（静态导出必备）
-  images: {
-    unoptimized: true,
-  },
+  // 静态导出
+  output: 'export',
+  // 关闭图片优化
+  images: { unoptimized: true },
+  // 路由兼容
+  trailingSlash: true,
 };
 
 export default nextConfig;
