d1ec142e37e6f81f7f6a1a45df0081edf1d9903b
alexchenxi <74758547+alexchenxi@users.noreply.github.com>
2026-03-18 01:24:58 +0000

    Deploying to gh-pages from @ alexchenxi/github-workflows-demo@b786b0f19203a5fc7ad3c1f85c761d0329c5016e 🚀

--- _next/static/Te2RlarI4GzQ_iVeeCOvr/_buildManifest.js
+++ _next/static/Te2RlarI4GzQ_iVeeCOvr/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/test/_next/:path+",
+        "destination": "/github-workflows-demo/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
